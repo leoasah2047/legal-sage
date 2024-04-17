71861d84a5e02fb2a761efc82c7a8a4a947eaad0
leoasah2047 <leoasah2047@gmail.com>
2024-04-17 07:47:56 +0100

    eslot

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   async redirects() {
     return [
       {
@@ -13,9 +16,6 @@ const nextConfig = {
         permanent: true,
       },
     ]
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 
   webpack: (
