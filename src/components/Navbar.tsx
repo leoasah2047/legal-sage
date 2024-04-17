e69092a30337f6cea2f47a5b625a49fcbdfd2ac5
leoasah2047 <leoasah2047@gmail.com>
2024-04-17 08:36:00 +0100

    looping

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,6 +14,7 @@ const Navbar = () => {
   const { getUser } = getKindeServerSession()
   const user = getUser()
 
+
   return (
     <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
       <MaxWidthWrapper>
